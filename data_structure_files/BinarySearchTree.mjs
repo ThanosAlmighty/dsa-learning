dc67b3c87bc2a8cac66501ef67a8bb18c0f70fd4
Jonathan Huling <jojohinky@gmail.com>
2026-04-11 15:47:17 -0400

    adding more DFS iterative traversal variations

--- data_structure_files/BinarySearchTree.mjs
+++ data_structure_files/BinarySearchTree.mjs
@@ -2,117 +2,117 @@ import BinaryNode from './BinaryNode.mjs';
 
 class BinarySearchTree {
 
-	constructor() {
-		this.root = null;
-	}
-	
-	insert(value) {
-	
-		if(value === null || value === undefined) {
-			return;
-		}
-	
-		if(this.root === null) {
-			this.root = new BinaryNode(value);
-			return;
-		}
-		
-		const [currentNode, parentNode] = this._lookupWithParent(value);
-		
-		if(currentNode !== null) {
-			return;
-		}
-		
-		if(parentNode.value < value) {
-			parentNode.right = new BinaryNode(value);
-		} else {
-			parentNode.left = new BinaryNode(value);
-		}
-	}
-	
-	remove(value) {
-	    const [nodeToRemove, parentOfNodeToRemove] = this._lookupWithParent(value);
-	    
-	    if(nodeToRemove) {
-	        let successor = nodeToRemove.right;
-	        
-	        if(successor) {
-	            if(successor.left) {
+    constructor() {
+        this.root = null;
+    }
+    
+    insert(value) {
+    
+        if(value === null || value === undefined) {
+            return;
+        }
+    
+        if(this.root === null) {
+            this.root = new BinaryNode(value);
+            return;
+        }
+        
+        const [currentNode, parentNode] = this._lookupWithParent(value);
+        
+        if(currentNode !== null) {
+            return;
+        }
+        
+        if(parentNode.value < value) {
+            parentNode.right = new BinaryNode(value);
+        } else {
+            parentNode.left = new BinaryNode(value);
+        }
+    }
+    
+    remove(value) {
+        const [nodeToRemove, parentOfNodeToRemove] = this._lookupWithParent(value);
+        
+        if(nodeToRemove) {
+            let successor = nodeToRemove.right;
+            
+            if(successor) {
+                if(successor.left) {
                     let successorParent;
-    	            while(successor.left) {
-    	                successorParent = successor;
-    	                successor = successor.left;
-    	            }
-    	            
-    	            const orphanNode = successor.right;
-    	            successorParent.left = orphanNode;
-    	            
-    	            successor.right = nodeToRemove.right;
-	            }
-	            
-	            successor.left = nodeToRemove.left;
-	        } else {
-	            successor = nodeToRemove.left;
-	        }
-	        
-	        if(parentOfNodeToRemove) {
-	            if(value > parentOfNodeToRemove.value) {
-    	            parentOfNodeToRemove.right = successor;
-    	        } else {
-    	            parentOfNodeToRemove.left = successor;
-    	        }
-	        } else {
-	            this.root = successor;
-	        }
-	    }
-	}
-	
-	_lookupWithParent(value) {
-	    let currentNode = this.root;
-		let parentNode = null;
-		
-		while(currentNode !== null && currentNode.value !== value) {
-			parentNode = currentNode;
-			currentNode = this._getNextByValue(value, currentNode);
-		}
-		
-		return [currentNode,parentNode];
-	}
-	
-	lookup(value) {
-		const [currentNode] = this._lookupWithParent(value);
-		
-		return currentNode;
-	}
-	
-	_lookupWithParent_recursive(value, currentNode = this.root, parentNode = null) {
-		if(currentNode === null || currentNode.value === value) {
-			return [currentNode, parentNode];
-		}
-		
-		return this._lookupWithParent_recursive(value, this._getNextByValue(value, currentNode), currentNode);
-	}
-	
-	lookup_recursive(value) {
-		const [currentNode] = this._lookupWithParent_recursive(value);
-		
-		return currentNode;
-	}
-	
-	_getNextByValue(value, currentNode) {
-		if(currentNode.value > value) {
-			return currentNode.left;
-		} else if (currentNode.value < value) {
-			return currentNode.right;
-		}
-		return currentNode;
-	}
-	
-	mapTreeToArray() {
-	    return this._mapTreeToArray(this.root, null);
-	}
-	
-	printTree() {
+                    while(successor.left) {
+                        successorParent = successor;
+                        successor = successor.left;
+                    }
+                    
+                    const orphanNode = successor.right;
+                    successorParent.left = orphanNode;
+                    
+                    successor.right = nodeToRemove.right;
+                }
+                
+                successor.left = nodeToRemove.left;
+            } else {
+                successor = nodeToRemove.left;
+            }
+            
+            if(parentOfNodeToRemove) {
+                if(value > parentOfNodeToRemove.value) {
+                    parentOfNodeToRemove.right = successor;
+                } else {
+                    parentOfNodeToRemove.left = successor;
+                }
+            } else {
+                this.root = successor;
+            }
+        }
+    }
+    
+    _lookupWithParent(value) {
+        let currentNode = this.root;
+        let parentNode = null;
+        
+        while(currentNode !== null && currentNode.value !== value) {
+            parentNode = currentNode;
+            currentNode = this._getNextByValue(value, currentNode);
+        }
+        
+        return [currentNode,parentNode];
+    }
+    
+    lookup(value) {
+        const [currentNode] = this._lookupWithParent(value);
+        
+        return currentNode;
+    }
+    
+    _lookupWithParent_recursive(value, currentNode = this.root, parentNode = null) {
+        if(currentNode === null || currentNode.value === value) {
+            return [currentNode, parentNode];
+        }
+        
+        return this._lookupWithParent_recursive(value, this._getNextByValue(value, currentNode), currentNode);
+    }
+    
+    lookup_recursive(value) {
+        const [currentNode] = this._lookupWithParent_recursive(value);
+        
+        return currentNode;
+    }
+    
+    _getNextByValue(value, currentNode) {
+        if(currentNode.value > value) {
+            return currentNode.left;
+        } else if (currentNode.value < value) {
+            return currentNode.right;
+        }
+        return currentNode;
+    }
+    
+    mapTreeToArray() {
+        return this._mapTreeToArray(this.root, null);
+    }
+    
+    printTree() {
         const treeArray = this.mapTreeToArray();
         
         console.log("Your tree: ")
@@ -122,8 +122,8 @@ class BinarySearchTree {
         
         console.log("\r\n")
     }
-	
-	_mapTreeToArray(node, parent, level = 0, tree = []) {
+    
+    _mapTreeToArray(node, parent, level = 0, tree = []) {
         if(node !== null && node !== undefined && node.value !== null) {
             tree[level] = (tree[level] === undefined ? `${node.value}` : `${tree[level]}, ${node.value}`) + `(${parent?.value ?? null})`;
             
@@ -161,6 +161,65 @@ class BinarySearchTree {
             
             if(curr) {
                 stack.push(curr);
+            }
+        }
+        
+        return list;
+    }
+    
+    traversePreOrder(startNode = this.root) {
+        const list = [startNode.value];
+        const stack = [startNode];
+        let curr = startNode;
+        
+        while(stack.length > 0) {
+            if(!curr) {
+                curr = stack.pop().right;
+            } else if(curr.left) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            } 
+            
+            if(curr) {
+                stack.push(curr);
+                list.push(curr.value);
+            }
+        }
+        
+        return list;
+    }
+    
+    traversePostOrder(startNode = this.root) {
+        const list = [];
+        const stack = [startNode];
+        const visitedChildren = new Set();    
+        let curr;
+        
+        while(stack.length > 0) {
+            curr = stack[stack.length - 1];
+            if(
+                (!curr.right || visitedChildren.has(curr.right)) 
+                && 
+                (!curr.left || visitedChildren.has(curr.left))
+            ) {
+              if(curr.right) {
+                visitedChildren.delete(curr.right);
+              }
+              if(curr.left) {
+                visitedChildren.delete(curr.left);
+              }
+              visitedChildren.add(curr);
+              list.push(curr.value);
+              stack.pop();
+              continue;
+            } else {
+                if(curr.right && !visitedChildren.has(curr.right)) {
+                    stack.push(curr.right);
+                } 
+                if(curr.left && !visitedChildren.has(curr.left)) {
+                    stack.push(curr.left);
+                }
             }
         }
         
